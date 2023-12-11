5a3d2c8c5fc6f049a301851bdc6819dfeb8cf6e2
Fagathron <stefan.alexandru093@gmail.com>
2023-12-11 16:01:50 +0200

    commit the first changes

---
+++ src/main/java/alex/gitdemo/GitDemo.java
@@ -0,0 +1,7 @@
+package alex.gitdemo;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Here's the new code added after cloning the project");
+    }
+}
